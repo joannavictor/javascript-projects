b8f84659a620a8e0228f6c8ece2bc4bfe7d95a19
joannavictor <joannamanoj20@gmail.com>
2024-01-17 22:53:48 -0600

    Array Exercise Completed

--- arrays/exercises/part-six-arrays.js
+++ arrays/exercises/part-six-arrays.js
@@ -1,11 +1,35 @@
 //Arrays can hold different data types, even other arrays! A multi-dimensional array is one with entries that are themselves arrays.
 
 //1) Define and initialize the arrays specified in the exercise to hold the name, chemical symbol and mass for different elements.
-
-//2) Define the array 'table', and use 'push' to add each of the element arrays to it. Print 'table' to see its structure.
-
+let element1 = ['hydrogen', 'H', 1.008];
+let element2 = ['helium', 'He', 4.003];
+let element26 = ['iron', 'Fe', 55.85];
+//2) Define the array 'table', and use 'push' to add each of the element arrays to it. 
+//Print 'table' to see its structure.
+let table = [];
+table.push(element1);
+table.push(element2);
+table.push(element26);
+console.log(table);
 //3) Use bracket notation to examine the difference between printing 'table' with one index vs. two indices (table[][]).
+console.log(element2[2]);
+console.log(table[2][2]);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
 
-//4) Using bracket notation and the table array, print the mass of element1, the name for element 2 and the symbol for element26.
+//4) Using bracket notation and the table array, 
+//print the mass of element1, the name for element 2 and the symbol for element26.
+console.log(table[0][2]);
+console.log(table[1][0]);
+console.log(table[2][1]);
+//5) 'table' is an example of a 2-dimensional array. 
+//The first “level” contains the element arrays, and the
+// second level holds the name/symbol/mass values. Experiment! 
+//Create a 3-dimensional array and print out one entry from each level in the array.
+let table3d = [];
+table3d.push(element2[2]);
+table3d.push(table);
+console.log(table3d);
 
-//5) 'table' is an example of a 2-dimensional array. The first “level” contains the element arrays, and the second level holds the name/symbol/mass values. Experiment! Create a 3-dimensional array and print out one entry from each level in the array.
+console.log(table3d[1][0][0]);
+console.log(table3d[1][1][2]);
+console.log(table3d[1][2][2]);
+
