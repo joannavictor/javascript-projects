ac1e1c449483cde1ab557e136e28a4e3c86124d7
joannavictor <joannamanoj20@gmail.com>
2024-01-18 20:57:48 -0600

    Array Testing - Studio - Part 2 Completed

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -3,27 +3,33 @@ let protoArray2 = "A;C;M;E";
 let protoArray3 = "space delimited string";
 let protoArray4 = "Comma-spaces, might, require, typing, caution";
 
+// I passed the above strings into array named "Strings" 
 strings = [protoArray1, protoArray2, protoArray3, protoArray4];
 
 //2) 
 function reverseCommas() {
 	//TODO: 1. create and instantiate your variables.
 	let check;
+	console.log(protoArray1.includes(","));
+	console.log(protoArray2.includes(";"));
+	console.log(protoArray3.includes(" "));
+	console.log(protoArray4.includes(", "));
+
 	let output;
 	//TODO: 2. write the code required for this step
-
+    output = strings[0].split(',').reverse().join(',');
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
-	return output;
+    return output;
 }
 
 //3)
 function semiDash() {
 	let check;
+	
 	let output;
 //TODO: write the code required for this step
-
-  
-	return output;
+    output = strings[1].split(';').sort().join('-');
+  	return output;
 }
 
 //4)
@@ -31,7 +37,7 @@ function reverseSpaces() {
 	let check;
 	let output;
   //TODO: write the code required for this step
-
+    output = (strings[2].split(' ').sort().reverse().join(' '));
 	return output;
 }
 
@@ -40,9 +46,9 @@ function commaSpace() {
 	let check;
 	let output;
 	//TODO: write the code required for this step
-  
+    output = strings[3].split(', ').reverse().join(',');
 	return output;
-}
+} 
 
 // NOTE: Don't add or modify any code below this line or your program might not run as expected.
 module.exports = {
