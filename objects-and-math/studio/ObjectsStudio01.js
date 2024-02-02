c55e7c12259a62c560f55fd6b4514fc9dd0c52ae
joannavictor <joannamanoj20@gmail.com>
2024-02-01 23:53:43 -0600

    Excercise - Objects

--- objects-and-math/studio/ObjectsStudio01.js
+++ objects-and-math/studio/ObjectsStudio01.js
@@ -1,4 +1,3 @@
-// Code your selectRandomEntry function here:
 
 
 // Code your buildCrewArray function here:
@@ -53,3 +52,68 @@ let candidateF = {
 let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
 
 // Code your template literal and console.log statements:
+
+// function to select a random entry from the idNumbers array. 
+function randomSelection(arr){
+  let index = Math.floor(Math.random()*arr.length);
+  return arr[index];
+};
+//Call the function three times to select three ID numbers. 
+//Store these selections in a new array, making sure to avoid repeated numbers. 
+//No animal can be selected more than once!
+let newArray = [];
+let randomId = 0;
+
+while(newArray < 3) {
+  randomId = randomSelection(idNumbers);
+  if (!newArray.includes(randomId)){
+       newArray.push(randomId);
+  }      
+};
+console.log(newArray);
+/*
+let selectedCrew= [];
+function buildCrewArray(ids,candidates) {
+for(i=0;i<ids.length;i++) {
+  for(j=0;j<candidates.length;j++) {
+    if(candidates[j].astronautID === ids[i]) {
+       selectedCrew.push(candidates[j]);
+
+       console.log(selectedCrew);
+    }
+    return selectedCrew;
+    }
+  }
+}
+
+let crew = buildCrewArray(newArray,animals);
+console.log(crew);
+
+// literal to print:
+//console.log(`${crew[0].name}, ${crew[1].name}, ${crew[2].name} "are going to space!" `);
+
+*/
+
+
+/*
+let newArray =[];
+// Code your selectRandomEntry function here:
+function randomSelection(array){
+  let store;
+ // newArray.push(array[Math.floor(Math.random()*array.length)]);
+  store =   array[Math.floor(Math.random()*array.length)];
+  if (newArray.includes(store)) {
+    return newArray}
+   else 
+   {
+    newArray.push(store); 
+   }
+  return newArray;
+  }
+ 
+while (newArray < 4) {
+  console.log(randomSelection(idNumbers));
+};
+
+
+*/
