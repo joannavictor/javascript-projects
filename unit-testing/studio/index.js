c0168127a1a9ff6c09647176477c0446eb284ebe
joannavictor <joannamanoj20@gmail.com>
2024-02-08 23:59:12 -0600

    studio complete

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,42 @@
 
 let launchcode = {
-  
-}
+    Organization:"non-profit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees:"100",
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput: function(num) {
+     let launchop = '';
+
+     if (num%2 == 0){launchop += 'Launch'; }
+     if (num%3 == 0){launchop += 'Code'; }
+     if (num%5 == 0){if (launchop){launchop += ' Rocks'; } else {launchop += 'Rocks!'}}
+     if (!launchop){return 'Rutabagas!'} else if (launchop === 'Launch Rocks'){
+        return launchop + '! (CRASH!!!)'} 
+     else
+        {return launchop};
+     
+    
+    
+     
+
+     
+
+
+
+
+
+     if (num%2 === 0 && num%3 === 0) { return "LauncCode!" }
+     else if (num%2 === 0) { return "Launch!" }
+     else if (num%3 === 0) { return "Code!" }
+     else if (num%5 === 0) { return "Rocks!" }  
+     else if (num%2 === 2 && num%3 === 3) { return "Code!" }
+     else if (num%2 === 3 && num === 5) { return "Code!" }
+     else if (num === 2 && num === 5 && num === 5) { return "Code!" }
+     else if (num === 3 && num === 3) { return "Code!" }
+     else if (num === 3 && num === 3) { return "Code!" }
+
+    }  
+} 
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,31 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
-  
+  test("organization is nonprofit", function() {
+       expect(launchcode.Organization).toBe("non-profit");
+  });
+  test("executiveDirector is Jeff", function() {
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  });
+  test("Percentage of CoolEmployees", function() {
+    expect(launchcode.percentageCoolEmployees).toBe("100");
+  });
+  test("Programs Offered", function() {
+    expect(launchcode.programsOffered[0]).toBe("Web Development");
+    expect(launchcode.programsOffered[1]).toBe("Data Analysis");
+    expect(launchcode.programsOffered[2]).toBe("Liftoff");
+    expect(launchcode.programsOffered.length).toBe(3);
+  });
+  test("When passed a number tas be divisible condition, Return Launch", function() {
+    expect(launchcode.launchOutput(2)).toBe("Launch");
+    expect(launchcode.launchOutput(3)).toBe("Code");
+    expect(launchcode.launchOutput(5)).toBe("Rocks!");
+ // expect(launchcode.launchOutput(10)).toBe("Launch Rocks!");
+    expect(launchcode.launchOutput(30)).toBe("LaunchCode Rocks");
+    expect(launchcode.launchOutput(15)).toBe("Code Rocks");
+    expect(launchcode.launchOutput(6)).toBe("LaunchCode");
+    expect(launchcode.launchOutput(10)).toBe("Launch Rocks! (CRASH!!!)");
+    expect(launchcode.launchOutput(7)).toBe("Rutabagas!");
+  }); 
+
 });
